0c4c929faaee1f82268e5c4392372393f8ddfe6e
Lys Minh <lyminh1994@gmail.com>
2023-12-05 00:01:00 +0700

    update NetworkImage to AssetImage

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'I am Rich',
-    home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey[900],
-        title: const Center(
-          child: Text(
-            'I am Rich',
-            style: TextStyle(color: Colors.white),
+  runApp(
+    MaterialApp(
+      title: 'I am Rich',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: const Center(
+            child: Text(
+              'I am Rich',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
-      body: const Center(
-        child: Image(
-          image: NetworkImage('https://picsum.photos/200'),
-        ),
-      ),
     ),
-  ));
+  );
 }
